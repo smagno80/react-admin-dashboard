c6c72efe8ef0d93a9cf8a3b292b77007673c673c
Pedro Camargo <sistemamagno@gmail.com>
2022-04-17 20:19:50 -0500

    Initial Commit Project created and updated repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <h1>Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
